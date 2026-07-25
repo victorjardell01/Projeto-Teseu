0f9416bed2c5a91f26537083ceea10423a679ecc
victorjardell01 <victor.jardell.01@gmail.com>
2026-07-25 02:33:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ function entrarComoClasse(classe) {
         window.location.href = "estudos/estudo.html";
         break;
       case "clerigo":
-        window.location.href = "santuario.html";
+        window.location.href = "santuario/santuario.html";
         break;
       case "druida":
         window.location.href = "druida/druida.html";
